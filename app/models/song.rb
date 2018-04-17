8220789fa4af83655644052517296c64a3df5213
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-17 04:39:29 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+
+end
