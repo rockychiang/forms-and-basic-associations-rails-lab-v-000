8f218cf6011df2a1003e9d2873ba0f5adc832e7f
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-17 04:45:12 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,9 +20,7 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents
-    self.notes.map do |note|
-      note.content
-    end
+    self.notes.map{|note| note.content}
   end
 
   def note_contents=(content)
